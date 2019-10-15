07d099eeef3c40b5b322bf43a35e0e746561c377
Utkarsh Bisaria <2utkarsh_bt2k16@dtu.ac.in>
2019-10-15 19:43:31 +0530

    Fetching Twitter Data, Classifying the Sentiment and Graphing Live Twitter Sentiment

---
+++ twitterapi.py
@@ -0,0 +1,7 @@
+ckey = "c2Hcccz3veVjH9FbY6UPgBM6X"
+
+csecret = "wVMbASCiRjfi6hi59tbXYBqUITROvBQwdPuzV1r40JmCm8E11K"
+
+atoken = "151932822-hUmm1sxgIVVrzW99ydO9eAWYDQpRGRux6Wrp5Ja9"
+
+asecret = "LpTGShHQlylaA3R4yzWGfFdkr372PSSb6eICKFfnXDUZv"
